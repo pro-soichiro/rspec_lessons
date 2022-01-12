c17976b02871b925b307f938be37eca2679c722d
pro-soichiro <dr.1987626@gmail.com>
2022-01-12 13:48:10 +0000

    [Update] #11 letを使ってみよう

--- lib/calc.rb
+++ lib/calc.rb
@@ -3,4 +3,8 @@ class Calc
     # 5 #仮実装
     a + b #明らかな実装
   end
+
+  def price(x,tax)
+    x * (1 + tax)
+  end
 end
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,7 +1,6 @@
 require "calc"
 
 RSpec.describe Calc do
-  # subject を使う
   subject(:calc) { Calc.new }
   it "addというメソッドがあるか" do
     expect(calc).to respond_to(:add)
@@ -17,5 +16,15 @@ RSpec.describe Calc do
   end
   it "出力される数字はINT型である" do
     expect(calc.add(3, 12)).to be_kind_of(Integer)
+  end
+
+  context "tax 5%" do
+    let(:tax) { 0.05 }
+    it { expect(calc.price(100, tax)).to eq(105) }
+  end
+
+  context "tax 8%" do
+    let(:tax) { 0.08 }
+    it { expect(calc.price(500, tax)).to eq(540) }
   end
 end
