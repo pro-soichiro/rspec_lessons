6957729c987b00fa02e020010b5fec9607f10108
pro-soichiro <dr.1987626@gmail.com>
2022-01-12 11:33:27 +0000

    [Update] #06 開発サイクルを進めよう

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,8 +1,15 @@
 require "calc"
 
 RSpec.describe "A calc" do
+  before do
+    @calc = Calc.new
+  end
+
   it "given 2 and 3, returns 5" do
-    calc = Calc.new
-    expect(calc.add(2, 3)).to eq(5)
+    expect(@calc.add(2, 3)).to eq(5)
+  end
+  # 三角測量
+  it "given 5 and 8, returns 13" do
+    expect(@calc.add(5, 8)).to eq(13)
   end
 end
