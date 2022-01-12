237d77544dba04112e7a829430cfb5b9d39a28f4
pro-soichiro <dr.1987626@gmail.com>
2022-01-12 13:37:12 +0000

    [Update] #10 subjectを使ってみよう

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,24 +1,21 @@
 require "calc"
 
 RSpec.describe Calc do
+  # subject を使う
+  subject(:calc) { Calc.new }
   it "addというメソッドがあるか" do
-    calc = Calc.new
     expect(calc).to respond_to(:add)
   end
   it "2+3は5になる" do
-    calc = Calc.new
     expect(calc.add(2, 3)).to eq(5)
   end
   it "3+3は5にならない" do
-    calc = Calc.new
     expect(calc.add(3, 3)).not_to eq(5)
   end
   it "3+12は10以上である" do
-    calc = Calc.new
     expect(calc.add(3, 12)).to be > 10
   end
   it "出力される数字はINT型である" do
-    calc = Calc.new
     expect(calc.add(3, 12)).to be_kind_of(Integer)
   end
 end
