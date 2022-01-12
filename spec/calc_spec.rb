1bc92a853a39ea0fd93cde5077969405f4a3eb71
pro-soichiro <dr.1987626@gmail.com>
2022-01-12 13:11:33 +0000

    [Update] #08 exampleを使ってみよう

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,14 +1,10 @@
 require "calc"
 
 RSpec.describe Calc do
-  context "when normal mode" do
-    it "given 2 and 3, returns 5" do
-      calc = Calc.new
-      expect(calc.add(2, 3)).to eq(5)
-    end
-  end
-  context "when graph mode" do
-    it "draws graph" do
-    end
-  end
+  # example / specify
+  it {
+    calc = Calc.new
+    expect(calc.add(2, 3)).to eq(5)
+  }
+  it "draws graph" # pending
 end
