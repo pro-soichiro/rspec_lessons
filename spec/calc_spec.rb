27e4d4dc935785abe7388575af9a35af1be5b448
pro-soichiro <dr.1987626@gmail.com>
2022-01-12 13:28:50 +0000

    [Update] #09 matcherを使ってみよう

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,10 +1,24 @@
 require "calc"
 
 RSpec.describe Calc do
-  # example / specify
-  it {
+  it "addというメソッドがあるか" do
+    calc = Calc.new
+    expect(calc).to respond_to(:add)
+  end
+  it "2+3は5になる" do
     calc = Calc.new
     expect(calc.add(2, 3)).to eq(5)
-  }
-  it "draws graph" # pending
+  end
+  it "3+3は5にならない" do
+    calc = Calc.new
+    expect(calc.add(3, 3)).not_to eq(5)
+  end
+  it "3+12は10以上である" do
+    calc = Calc.new
+    expect(calc.add(3, 12)).to be > 10
+  end
+  it "出力される数字はINT型である" do
+    calc = Calc.new
+    expect(calc.add(3, 12)).to be_kind_of(Integer)
+  end
 end
