bdd3a7e7eef85213b876b4f6273335cb5aed0435
pro-soichiro <dr.1987626@gmail.com>
2022-01-12 13:57:08 +0000

    [Update] #12 letとlet!を使い分けよう

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,10 +1,10 @@
 class Calc
-  def add(a, b)
-    # 5 #仮実装
-    a + b #明らかな実装
+  attr_accessor :tax
+  def initialize
+    @tax = 0
   end
 
-  def price(x,tax)
+  def price(x)
     x * (1 + tax)
   end
 end
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -2,29 +2,11 @@ require "calc"
 
 RSpec.describe Calc do
   subject(:calc) { Calc.new }
-  it "addというメソッドがあるか" do
-    expect(calc).to respond_to(:add)
-  end
-  it "2+3は5になる" do
-    expect(calc.add(2, 3)).to eq(5)
-  end
-  it "3+3は5にならない" do
-    expect(calc.add(3, 3)).not_to eq(5)
-  end
-  it "3+12は10以上である" do
-    expect(calc.add(3, 12)).to be > 10
-  end
-  it "出力される数字はINT型である" do
-    expect(calc.add(3, 12)).to be_kind_of(Integer)
-  end
 
-  context "tax 5%" do
-    let(:tax) { 0.05 }
-    it { expect(calc.price(100, tax)).to eq(105) }
-  end
+  let!(:tax) {
+    calc.tax = 0.05
+  }
 
-  context "tax 8%" do
-    let(:tax) { 0.08 }
-    it { expect(calc.price(500, tax)).to eq(540) }
-  end
+  it { expect(tax).to eq(0.05) }
+  it { expect(calc.price(100)).to eq(105) }
 end
